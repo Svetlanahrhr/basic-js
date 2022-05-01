33f655d41e21deb579662a6ff73f7ccc5aa645ea
Svetlanahrhr <kovalenkohp@mail.ru>
2022-05-01 15:50:04 +0400

    feat: finish dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,29 +1,31 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
- function createDreamTeam(members) {
+function createDreamTeam(members) {
   let sum = [];
-  for (let i = 0; i < members.length; i++) {
-    if (typeof members[i] !== 'string') {
-      return false
-    } else {sum.push(members[i].slice(0, 1).toUpperCase())}
-  }
-  sum.sort()
-  return sum.join("").trim();
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+        sum.push(members[i].trim().slice(0, 1).toUpperCase());
+      }
+    }
+    sum.sort();
+    return sum.join("").trim();
+  } else return false;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
