c6ceb246a09f89054995bb8580cf08edcd6a1c70
Svetlanahrhr <maadlana@mail.ru>
2022-10-14 16:35:52 +0400

    feat: add encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,26 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
- function encodeLine( s1 ) {
-  s1 = s1.split('')
-  let sum = ''
-  let some1 = {}
-  let length1 = s1.length
- for (let i=0; i<length1; i++) {
-   if (!some1[`${s1[i]}`]) {
-     some1[`${s1[i]}`] = 1
-   } else {
-    some1[`${s1[i]}`]++
-   }
- }
- for (key1 in some1) {
-   console.log(some1[key1]);
-   console.log(key1);
-   if (some1[key1] > 1) {
-    sum += String(some1[key1])+String(key1)+''
-   } else sum += String(key1)+""
- }
- return sum
+ function encodeLine(s1) {
+  s1 = s1.split("");
+  let sum = "";
+  let some1 = {};
+  let length1 = s1.length;
+  for (let i = 1; i <= length1; i++) {
+    if (!some1[`${s1[i - 1]}`] && s1[i - 1] !== s1[i]) {
+      sum += String(s1[i - 1]) + "";
+    } else if (!some1[`${s1[i - 1]}`]) {
+      some1[`${s1[i - 1]}`] = 1;
+    } else if (s1[i - 1] === s1[i]) {
+      some1[`${s1[i - 1]}`]++;
+    } else if (s1[i - 1] !== s1[i] || i === length1) {
+      some1[`${s1[i - 1]}`]++;
+      sum += String(some1[s1[i - 1]]) + String(s1[i - 1]) + "";
+      console.log(some1);
+      some1 = {};
+    }
+  }
+
+  return sum;
 }
 
 module.exports = {
