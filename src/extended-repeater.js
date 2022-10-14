937551e878ff0f7baa88fb64e4f994e4f1b5eaf7
Svetlanahrhr <maadlana@mail.ru>
2022-10-14 14:28:26 +0400

    feat: add ad separator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,37 +1,44 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
  function repeater(str, options) {
-  let sum = ''
-if (options.repeatTimes && options.separator && options.addition && options.additionRepeatTimes && options.additionSeparator) {
+  str = ""+str;
+  let sum = "";
+  options.repeatTimes ? (options.repeatTimes = options.repeatTimes) : (options.repeatTimes = 1);
+  options.separator ? (options.separator = options.separator) : (options.separator = "+");
+  options.addition ||  options.addition === false || options.addition === null ? (options.addition = options.addition+'') : (options.addition = "");
+  options.additionRepeatTimes ? (options.additionRepeatTimes = options.additionRepeatTimes) : (options.additionRepeatTimes = 1);
+  options.additionSeparator ? (options.additionSeparator = options.additionSeparator) : (options.additionSeparator = "|");
+
   for (let i = 0; i < options.repeatTimes; i++) {
-      sum+=str
-      for (let i = 0; i < options.additionRepeatTimes; i++) {
-      sum+=options.addition+options.additionSeparator
+    sum += str;
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      sum += options.addition;
+      if (i !== options.additionRepeatTimes - 1) {
+        sum += options.additionSeparator;
       }
-      if (i !== options.repeatTimes) {
-          sum+=options.separator
-      }
-      
+    }
+    if (i !== options.repeatTimes - 1) {
+      sum += options.separator;
+    }
   }
-}
-console.log(sum);
+  return sum;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
