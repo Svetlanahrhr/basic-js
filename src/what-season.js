7ef2eb2514f952f15090f2ba220a899faee0b182
Svetlanahrhr <maadlana@mail.ru>
2022-10-16 21:06:59 +0400

    feat: add date

--- src/what-season.js
+++ src/what-season.js
@@ -1,49 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
- function getSeason(date) {
-  if (Object.prototype.toString.call(date) !== '[object Date]') {
-    throw new Error("Invalid date!")
-   }
-  let month = date.getMonth()
+function getSeason(date = "no-value") {
+  if (date === "no-value") {
+    return "Unable to determine the time of year!";
+  }
+  if (Object.prototype.toString.call(date) !== "[object Date]" || !(date instanceof Date)) {
+    throw new Error("Invalid date!");
+  }
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length) {
+    throw new Error("Invalid date!");
+  }
+  let month = date.getMonth();
   switch (month) {
-    case 11: 
-    return 'winter'
-    case 0: 
-    return 'winter'
-    case 1: 
-    return 'winter'
-    case 2: 
-    return 'spring'
-    case 3: 
-    return 'spring'
-    case 4: 
-    return 'spring'
-    case 5: 
-    return 'summer'
-    case 6: 
-    return 'summer'
-    case 7: 
-    return 'summer'
-    case 8: 
-    return 'autumn'
-    case 9: 
-    return 'autumn'
-    case 10: 
-    return 'autumn'
+    case 11:
+      return "winter";
+    case 0:
+      return "winter";
+    case 1:
+      return "winter";
+    case 2:
+      return "spring";
+    case 3:
+      return "spring";
+    case 4:
+      return "spring";
+    case 5:
+      return "summer";
+    case 6:
+      return "summer";
+    case 7:
+      return "summer";
+    case 8:
+      return "autumn";
+    case 9:
+      return "autumn";
+    case 10:
+      return "autumn";
   }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
